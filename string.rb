f48d1c87dcb1b174d17f60047b59526bb97c1999
kisumutalent <homiemusa@gmail.com>
2019-01-21 18:19:51 +0300

    Ruby basic

---
+++ string.rb
@@ -0,0 +1,20 @@
+# puts "refue" + " " + "wise"
+
+puts "Enter your first name"
+first_name = gets.chomp
+puts "Enter your last name"
+last_name = gets.chomp
+puts "Enter your age"
+age = gets.chomp
+puts "Welcome #{first_name}, #{last_name} to the playround"
+puts "Your age is #{age} and length of your first name is #{first_name.length}"
+
+puts "Enter your first number"
+first_number = gets.chomp
+puts "Enter your last number"
+second_number = gets.chomp
+
+puts "The first number multplied by the second number is #{first_number.to_i * second_number.to_i}"
+puts "The devision of first number and second number is #{first_number.to_i / second_number.to_i}"
+puts "The mode of first number and second number is #{first_number.to_i % second_number.to_i}"
+puts "The Addation of first number and second number is #{first_number.to_i + second_number.to_i}"
